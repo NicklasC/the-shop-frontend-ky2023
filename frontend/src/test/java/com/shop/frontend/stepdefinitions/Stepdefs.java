c671f7d279446602e05065a91d7b5864ffff0fd2
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-05-10 12:15:00 +0200

    Add initial testcase

---
+++ frontend/src/test/java/com/shop/frontend/stepdefinitions/Stepdefs.java
@@ -0,0 +1,69 @@
+package com.shop.frontend.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Stepdefs {
+
+    public static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("incognito");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println("Setting up....");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        System.out.println("Closing down....");
+        driver.quit();
+    }
+
+    @Given("user visits The Shop mainpage")
+    public void userVisitsTheShopMainpage() {
+        driver.get("https://webshop-agil-testautomatiserare.netlify.app/");//Before each normally navigates to page, but doing it here as well for better test understandability.
+    }
+
+    @Then("the browser title should be {string}")
+    public void theBrowserTitleShouldBe(String expectedTitle) {
+        assertEquals(expectedTitle, driver.getTitle(), "Page title is not as expected");
+    }
+
+
+    // method below can be used for utility purposes during test creation, error searching and simiar
+    public static void sleep(int numberOfSeconds) {
+        try {
+            Thread.sleep(numberOfSeconds * 1000); // Sleep for number of seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
