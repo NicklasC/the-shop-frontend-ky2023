02c8a7e6ccb2d05bad103a8302c219b8d0f48921
pritut92 <94788040+pritut92@users.noreply.github.com>
2023-05-24 22:21:27 +0200

    TA-82 Added Product-page-tests.feature and ProductPage class

---
+++ src/test/java/com/shop/frontend/pages/ProductPage.java
@@ -0,0 +1,11 @@
+package com.shop.frontend.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductPage {
+
+    WebDriver driver;
+
+    public ProductPage(WebDriver driver) { this.driver = driver; }
+
+}
