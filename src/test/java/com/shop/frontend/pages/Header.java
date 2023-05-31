09892f353b9c6d42db0408459d231fd42e44fada
Camilla <73700387+millifrill@users.noreply.github.com>
2023-05-31 20:32:50 +0200

    TA-68: added author comment

--- src/test/java/com/shop/frontend/pages/Header.java
+++ src/test/java/com/shop/frontend/pages/Header.java
@@ -7,26 +7,32 @@ import org.openqa.selenium.WebElement;
 public class Header {
     WebDriver driver;
 
+    // Author: Camilla
     By link_home = By.xpath("/html/body/header/div/div/ul/li[1]/a");
+    // Author: Camilla
     By link_shop = By.xpath("/html/body/header/div/div/ul/li[2]/a");
 
 
+    // Author: Camilla
     public Header(WebDriver driver){
         this.driver = driver;
     }
 
+    // Author: Camilla
     public WebElement clickHomeLink() {
         WebElement homeLink = driver.findElement(link_home);
         homeLink.click();
         return homeLink;
     }
 
+    // Author: Camilla
     public WebElement clickShopLink() {
         WebElement shopLink = driver.findElement(link_shop);
         shopLink.click();
         return shopLink;
     }
 
+    // Author: Camilla
     public String getCurrentURL() {
         return driver.getCurrentUrl();
     }
