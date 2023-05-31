28de52e7e3c1743ba6953bb0dfa917d5d5dbfe6d
pritut92 <94788040+pritut92@users.noreply.github.com>
2023-05-31 13:59:30 +0200

    TA-87 Created test for Check Men's Clothing Category in ProductPage

--- src/test/java/com/shop/frontend/pages/ProductPage.java
+++ src/test/java/com/shop/frontend/pages/ProductPage.java
@@ -1,12 +1,19 @@
 package com.shop.frontend.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.Collections;
+import java.util.List;
 
 public class ProductPage {
     WebDriver driver;
     // Author: Priyanka
+    By btn_Shop = By.xpath("/html/body/header/div/div/ul/li[2]/a");
+    By txt_SearchField = By.id("search");
+    By productList = By.className("col");
     By tabMensClothing = By.xpath("/html/body/div[1]/div/ul/li[2]/a");
 
     public ProductPage(WebDriver driver) {
@@ -14,7 +21,26 @@ public class ProductPage {
     }
 
     // Author: Priyanka
-    public WebElement clickOnMensClothingTab(){
+
+    public void clickOnButtonShop() {
+        WebElement element = driver.findElement(btn_Shop);
+        element.click();
+    }
+
+    // Author: Priyanka
+    public void enterDataInSearchField(String value) {
+        WebElement element = driver.findElement(txt_SearchField);
+        element.sendKeys(value);
+        element.sendKeys(Keys.ENTER);
+    }
+
+    // Author: Priyanka
+    public List<WebElement> getProductList() {
+        List<WebElement> products = Collections.singletonList(driver.findElement(productList));
+        return products;
+    }
+
+    public WebElement clickOnMensClothingTab() {
         WebElement TabMensClothing = driver.findElement(tabMensClothing);
         TabMensClothing.click();
         return TabMensClothing;
--- src/test/resources/features/Product-page-tests.feature
+++ src/test/resources/features/Product-page-tests.feature
@@ -1,6 +1,11 @@
 Feature: Test product page elements and functionality
 
   // Author: Priyanka
+  Scenario: Validate click on Search Option
+    Given Visit The Shop Website
+    When Click on the Shop tab
+    And Search the product
+    Then Product should be displayed
 
   Scenario: Validate click on Men's Clothing Tab
     Given Visit The Shop Website
